31282aa94aa311ec465607e8308cbdce7bf28028
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-01-15 11:05:14 +0700

    fix: index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import app from "./app";
 import config from "./utils/env";
 
-const PORT = Number(config.PORT) || 5000;
-const HOST = config.HOST;
 
-app.listen(PORT, HOST, () => {
+app.listen(config.PORT, () => {
   console.log(`🚀 Server running in ${config.NODE_ENV} mode`);
-  console.log(`📡 Host: ${HOST}:${PORT}`);
   
-  if (config.NODE_ENV === 'development') {
-    console.log(`🔗 Local: http://localhost:${PORT}`);
-  }
 });
